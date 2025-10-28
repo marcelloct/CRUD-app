6859c06dbb1356d4c0b0fe59a23b50ae5739bad8
Marcello C <marcello_ct@hotmail.com>
2025-10-28 18:23:11 -0300

    public/

---
+++ public/js/main.js
@@ -0,0 +1,30 @@
+const deleteText = document.querySelectorAll(".fa-trash");
+const thumbText = document.querySelectorAll(".fa-thumbs-up");
+
+Array.from(deleteText).forEach((e) => {
+  e.addEventListener("click", deleteMovie);
+});
+
+Array.from(thumbText).forEach((e) => {
+  e.addEventListener("click", addLike);
+});
+
+async function deleteMovie() {
+  const t = this.parentNode.childNodes[1].innerText;
+  const g = this.parentNode.childNodes[3].innerText;
+  try {
+    const response = await fetch("deleteMovie", {
+      method: "delete",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title: t,
+        genre: g,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+    location.reload();
+  } catch (err) {
+    console.error(err);
+  }
+}
